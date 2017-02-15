b75af5c3ab4bf4505194512f31c21dd0c48b0e44
William A Sims III <willsims14@gmail.com>
2017-02-15 11:45:46 -0600

    Refactored new repository

--- javascripts/firebaseConfig.js
+++ javascripts/firebaseConfig.js
@@ -2,12 +2,17 @@
 let firebase = require("firebase/app"),
     fb = require("./fb-getter"),
     fbData = fb();
+
+   
 require("firebase/auth");
 require("firebase/database");
+
+
 var config = {
   apiKey: fbData.apiKey,
   databaseURL: fbData.databaseURL,
   authDomain: fbData.authDomain
 };
+
 firebase.initializeApp(config);
 module.exports = firebase;
