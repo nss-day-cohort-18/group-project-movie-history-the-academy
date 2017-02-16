074717f5fe964835ddce0cd05c7ea55203375b89
William A Sims III <willsims14@gmail.com>
2017-02-16 09:40:35 -0600

    Search functionality added.

---
+++ javascripts/api-interaction.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let $ = require('jquery');
+let config = require("./firebaseConfig.js");
+
+
+function searchFor(searchResult) {
+
+	console.log("searchResult", searchResult);
+	return new Promise(function(resolve,reject){
+		$.ajax({
+	    	url:`https://api.themoviedb.org/3/search/movie?api_key=e891491a45e46c6e7b42d5373e731739&query=${searchResult}`
+		}).done(function(movieData){
+    		resolve(movieData);
+  		}).fail( function(error){
+  			console.log("API ERRPR");
+  			reject(error);
+  		});
+	});
+}
+
+module.exports = {searchFor};
